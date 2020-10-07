b9675c3b5f6821ffffbcf6c0ecb6895b12770e1a
mhaas20 <mhaas@miuandes.cl>
2020-10-06 21:23:57 -0300

    inicial

---
+++ Manejo de excepciones/Manejo de excepciones/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_de_excepciones
+{
+    class User
+    {
+        private string name;
+        private string balance;
+
+        public User()
+        {
+
+        }
+        public void ChangeBalance(int money)
+        {
+
+        }
+       
+        
+    }
+
+    
+}
